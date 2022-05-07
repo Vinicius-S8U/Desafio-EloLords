c466c5b0d2188ae41e6e20e41c549f40ce607758
Vinicius S Barbosa <vinicius.barbosa@sptech.school>
2022-05-07 18:54:56 -0300

    tela de cadastro e alterações no index

--- Script/Script.js
+++ Script/Script.js
@@ -5,94 +5,87 @@ function MudeimgA() {
     img_elo_currentA = document.getElementById('img_elo_atual')
 
     if (elo_atual == "f" & nivel_atual == 1) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Ferro_1.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ferro_1.png`
     }
     else if (elo_atual == "f" & nivel_atual == 2) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Ferro_2.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ferro_2.png`
     }
     else if (elo_atual == "f" & nivel_atual == 3) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Ferro_3.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ferro_3.png`
     }
     else if (elo_atual == "f" & nivel_atual == 4) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Ferro_4.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ferro_4.png`
     }
 
     if (elo_atual == "b" & nivel_atual == 1) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Bronze_1.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Bronze_1.png`
     }
     else if (elo_atual == "b" & nivel_atual == 2) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Bronze_2.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Bronze_2.png`
     }
     else if (elo_atual == "b" & nivel_atual == 3) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Bronze_3.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Bronze_3.png`
     }
     else if (elo_atual == "b" & nivel_atual == 4) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Bronze_4.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Bronze_4.png`
     }
 
     if (elo_atual == "pr" & nivel_atual == 1) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Prata_1.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Prata_1.png`
     }
     else if (elo_atual == "pr" & nivel_atual == 2) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Prata_2.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Prata_2.png`
     }
     else if (elo_atual == "pr" & nivel_atual == 3) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Prata_3.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Prata_3.png`
     }
     else if (elo_atual == "pr" & nivel_atual == 4) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Prata_4.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Prata_4.png`
     }
 
     if (elo_atual == "o" & nivel_atual == 1) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Ouro_1.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ouro_1.png`
     }
     else if (elo_atual == "o" & nivel_atual == 2) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Ouro_2.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ouro_2.png`
     }
     else if (elo_atual == "o" & nivel_atual == 3) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Ouro_3.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ouro_3.png`
     }
     else if (elo_atual == "o" & nivel_atual == 4) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Ouro_4.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ouro_4.png`
     }
 
     if (elo_atual == "p" & nivel_atual == 1) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Platina_1.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Platina_1.png`
     }
     else if (elo_atual == "p" & nivel_atual == 2) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Platina_2.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Platina_2.png`
     }
     else if (elo_atual == "p" & nivel_atual == 3) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Platina_3.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Platina_3.png`
     }
     else if (elo_atual == "p" & nivel_atual == 4) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Platina_4.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Platina_4.png`
     }
 
     if (elo_atual == "d" & nivel_atual == 1) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Diamante_1.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Diamante_1.png`
     }
     else if (elo_atual == "d" & nivel_atual == 2) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Diamante_2.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Diamante_2.png`
     }
     else if (elo_atual == "d" & nivel_atual == 3) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Diamante_3.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Diamante_3.png`
     }
     else if (elo_atual == "d" & nivel_atual == 4) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Diamante_4.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Diamante_4.png`
     }
 
     if (elo_atual == "m") {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Mestre_1.png`
+        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Mestre_1.png`
     }
 
-    if (elo_atual == "g") {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/grao-mestre.webp`
-    }
-
-    if (elo_atual == "de") {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/J6-zqzk9_400x400.png`
-    }
 }
 
 function MudeimgB() {
@@ -102,92 +95,94 @@ function MudeimgB() {
     img_elo_currentB = document.getElementById('img_elo_desejavel')
 
     if (elo_desejado == "f" & nivel_desejado == 1) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Ferro_1.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ferro_1.png`
     }
     else if (elo_desejado == "f" & nivel_desejado == 2) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Ferro_2.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ferro_2.png`
     }
     else if (elo_desejado == "f" & nivel_desejado == 3) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Ferro_3.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ferro_3.png`
     }
     else if (elo_desejado == "f" & nivel_desejado == 4) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Ferro_4.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ferro_4.png`
     }
 
     if (elo_desejado == "b" & nivel_desejado == 1) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Bronze_1.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Bronze_1.png`
     }
     else if (elo_desejado == "b" & nivel_desejado == 2) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Bronze_2.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Bronze_2.png`
     }
     else if (elo_desejado == "b" & nivel_desejado == 3) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Bronze_3.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Bronze_3.png`
     }
     else if (elo_desejado == "b" & nivel_desejado == 4) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Bronze_4.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Bronze_4.png`
     }
 
     if (elo_desejado == "pr" & nivel_desejado == 1) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Prata_1.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Prata_1.png`
     }
     else if (elo_desejado == "pr" & nivel_desejado == 2) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Prata_2.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Prata_2.png`
     }
     else if (elo_desejado == "pr" & nivel_desejado == 3) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Prata_3.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Prata_3.png`
     }
     else if (elo_desejado == "pr" & nivel_desejado == 4) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Prata_4.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Prata_4.png`
     }
 
     if (elo_desejado == "o" & nivel_desejado == 1) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Ouro_1.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ouro_1.png`
     }
     else if (elo_desejado == "o" & nivel_desejado == 2) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Ouro_2.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ouro_2.png`
     }
     else if (elo_desejado == "o" & nivel_desejado == 3) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Ouro_3.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ouro_3.png`
     }
     else if (elo_desejado == "o" & nivel_desejado == 4) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Ouro_4.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ouro_4.png`
     }
 
     if (elo_desejado == "p" & nivel_desejado == 1) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Platina_1.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Platina_1.png`
     }
     else if (elo_desejado == "p" & nivel_desejado == 2) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Platina_2.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Platina_2.png`
     }
     else if (elo_desejado == "p" & nivel_desejado == 3) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Platina_3.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Platina_3.png`
     }
     else if (elo_desejado == "p" & nivel_desejado == 4) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Platina_4.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Platina_4.png`
     }
 
     if (elo_desejado == "d" & nivel_desejado == 1) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Diamante_1.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Diamante_1.png`
     }
     else if (elo_desejado == "d" & nivel_desejado == 2) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Diamante_2.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Diamante_2.png`
     }
     else if (elo_desejado == "d" & nivel_desejado == 3) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Diamante_3.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Diamante_3.png`
     }
     else if (elo_desejado == "d" & nivel_desejado == 4) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Diamante_4.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Diamante_4.png`
     }
 
     if (elo_desejado == "m") {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/Mestre_1.png`
+        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Mestre_1.png`
     }
 
-    if (elo_desejado == "g") {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/grao-mestre.webp`
-    }
+ 
 
-    if (elo_desejado == "de") {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/Img e Itens/Elos/J6-zqzk9_400x400.png`
-    }
+
+}
+
+
+function Goback(){
+window.location.assign("Index.html") 
+
 }
