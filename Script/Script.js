0d168950f94320e7d6e145e74b4c648a58360f27
Vinicius S Barbosa <vinicius.barbosa@sptech.school>
2022-05-11 23:34:46 -0300

    Kevyn e suas ideias sobre CSS

--- Script/Script.js
+++ Script/Script.js
@@ -23,113 +23,113 @@ function MudeimgA() {
 
 
     if (elo_atual == "Ferro" & nivel_atual == 1) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ferro_1.png`  
+        img_elo_currentA.src = `/ImgDeItens/Elos/Ferro_1.png`  
       span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
     else if (elo_atual == "Ferro" & nivel_atual == 2) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ferro_2.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Ferro_2.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}` 
     }
     else if (elo_atual == "Ferro" & nivel_atual == 3) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ferro_3.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Ferro_3.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}` 
     }
     else if (elo_atual == "Ferro" & nivel_atual == 4) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ferro_4.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Ferro_4.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}` 
     }
 
     if (elo_atual == "Bronze" & nivel_atual == 1) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Bronze_1.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Bronze_1.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
     else if (elo_atual == "Bronze" & nivel_atual == 2) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Bronze_2.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Bronze_2.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
     else if (elo_atual == "Bronze" & nivel_atual == 3) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Bronze_3.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Bronze_3.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
     else if (elo_atual == "Bronze" & nivel_atual == 4) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Bronze_4.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Bronze_4.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
 
     if (elo_atual == "Prata" & nivel_atual == 1) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Prata_1.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Prata_1.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}` 
     }
     else if (elo_atual == "Prata" & nivel_atual == 2) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Prata_2.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Prata_2.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}` 
     }
     else if (elo_atual == "Prata" & nivel_atual == 3) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Prata_3.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Prata_3.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}` 
     }
     else if (elo_atual == "Prata" & nivel_atual == 4) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Prata_4.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Prata_4.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}` 
     }
 
     if (elo_atual == "Ouro" & nivel_atual == 1) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ouro_1.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Ouro_1.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`  
     }
     else if (elo_atual == "Ouro" & nivel_atual == 2) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ouro_2.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Ouro_2.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`  
     }
     else if (elo_atual == "Ouro" & nivel_atual == 3) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ouro_3.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Ouro_3.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`  
     }
     else if (elo_atual == "Ouro" & nivel_atual == 4) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ouro_4.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Ouro_4.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`  
     }
 
     if (elo_atual == "Platina" & nivel_atual == 1) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Platina_1.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Platina_1.png`
       span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
     else if (elo_atual == "Platina" & nivel_atual == 2) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Platina_2.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Platina_2.png`
       span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
     else if (elo_atual == "Platina" & nivel_atual == 3) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Platina_3.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Platina_3.png`
       span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
     else if (elo_atual == "Platina" & nivel_atual == 4) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Platina_4.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Platina_4.png`
       span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
 
     if (elo_atual == "Diamante" & nivel_atual == 1) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Diamante_1.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Diamante_1.png`
         span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
 
     }
     else if (elo_atual == "Diamante" & nivel_atual == 2) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Diamante_2.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Diamante_2.png`
         span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
 
     }
     else if (elo_atual == "Diamante" & nivel_atual == 3) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Diamante_3.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Diamante_3.png`
         span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
 
     }
     else if (elo_atual == "Diamante" & nivel_atual == 4) {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Diamante_4.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Diamante_4.png`
         span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
 
     }
 
     if (elo_atual == "Mestre") {
-        img_elo_currentA.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Mestre_1.png`
+        img_elo_currentA.src = `/ImgDeItens/Elos/Mestre_1.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
 
@@ -155,113 +155,113 @@ function MudeimgB() {
 
 
     if (elo_desejado == "Ferro" & nivel_desejado == 1) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ferro_1.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Ferro_1.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}` 
     }
     else if (elo_desejado == "Ferro" & nivel_desejado == 2) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ferro_2.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Ferro_2.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}` 
     }
     else if (elo_desejado == "Ferro" & nivel_desejado == 3) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ferro_3.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Ferro_3.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}` 
     }
     else if (elo_desejado == "Ferro" & nivel_desejado == 4) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ferro_4.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Ferro_4.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}` 
     }
 
     if (elo_desejado == "Bronze" & nivel_desejado == 1) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Bronze_1.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Bronze_1.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
     else if (elo_desejado == "Bronze" & nivel_desejado == 2) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Bronze_2.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Bronze_2.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
     else if (elo_desejado == "Bronze" & nivel_desejado == 3) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Bronze_3.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Bronze_3.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
     else if (elo_desejado == "Bronze" & nivel_desejado == 4) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Bronze_4.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Bronze_4.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
 
     if (elo_desejado == "Prata" & nivel_desejado == 1) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Prata_1.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Prata_1.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}` 
     }
     else if (elo_desejado == "Prata" & nivel_desejado == 2) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Prata_2.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Prata_2.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}` 
     }
     else if (elo_desejado == "Prata" & nivel_desejado == 3) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Prata_3.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Prata_3.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}` 
     }
     else if (elo_desejado == "Prata" & nivel_desejado == 4) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Prata_4.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Prata_4.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}` 
     }
 
     if (elo_desejado == "Ouro" & nivel_desejado == 1) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ouro_1.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Ouro_1.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`  
     }
     else if (elo_desejado == "Ouro" & nivel_desejado == 2) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ouro_2.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Ouro_2.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`  
     }
     else if (elo_desejado == "Ouro" & nivel_desejado == 3) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ouro_3.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Ouro_3.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`  
     }
     else if (elo_desejado == "Ouro" & nivel_desejado == 4) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Ouro_4.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Ouro_4.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`  
     }
 
     if (elo_desejado == "Platina" & nivel_desejado == 1) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Platina_1.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Platina_1.png`
       span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
     else if (elo_desejado == "Platina" & nivel_desejado == 2) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Platina_2.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Platina_2.png`
       span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
     else if (elo_desejado == "Platina" & nivel_desejado == 3) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Platina_3.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Platina_3.png`
       span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
     else if (elo_desejado == "Platina" & nivel_desejado == 4) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Platina_4.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Platina_4.png`
       span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
 
     if (elo_desejado == "Diamante" & nivel_desejado == 1) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Diamante_1.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Diamante_1.png`
               span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
 
     }
     else if (elo_desejado == "Diamante" & nivel_desejado == 2) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Diamante_2.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Diamante_2.png`
               span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
 
     }
     else if (elo_desejado == "Diamante" & nivel_desejado == 3) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Diamante_3.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Diamante_3.png`
               span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
 
     }
     else if (elo_desejado == "Diamante" & nivel_desejado == 4) {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Diamante_4.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Diamante_4.png`
               span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
 
     }
 
     if (elo_desejado == "Mestre") {
-        img_elo_currentB.src = `/Desafio-EloLorde/Desafio-EloLords/ImgDeItens/Elos/Mestre_1.png`
+        img_elo_currentB.src = `/ImgDeItens/Elos/Mestre_1.png`
        span_correto.innerHTML = `Valor do ${elo_atual} ${nivel_atual} ao ${elo_desejado} ${nivel_desejado}`
     }
 
